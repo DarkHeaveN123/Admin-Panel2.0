40689dc0b6edce03d42b779676aa073d6fb332fb
DARKHEAVEN <79021904+DarkHeaveN123@users.noreply.github.com>
2021-02-14 18:12:05 +0530

    Create install.sh

---
+++ Darkheaven/install.sh
@@ -0,0 +1,31 @@
+Installer logs
+pip-log.txt
+pip-delete-this-directory.txt
+
+# Unit test / coverage reports
+htmlcov/
+.tox/
+.nox/
+.coverage
+.coverage.*
+.cache
+nosetests.xml
+coverage.xml
+*.cover
+*.py,cover
+.hypothesis/
+.pytest_cache/
+
+# Translations
+*.mo
+*.pot
+
+# Django stuff:
+*.log
+local_settings.py
+db.sqlite3
+db.sqlite3-journal
+
+# Flask stuff:
+instance/
+.webassets-cache
